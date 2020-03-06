5d26556fbcc2038e5ab65ee08b4e97970e6fe8e0
vlad <gilbert.cook111@gmail.com>
2020-03-06 23:39:09 +0300

    all axios requests transfered to API file using axios create method to create instance for refactoring

---
+++ src/api/api.js
@@ -0,0 +1,30 @@
+import * as axios from 'axios';
+
+let instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        "API-KEY": '8e9ee1e6-5deb-41eb-a54b-cd03fc3e6240',
+    },
+})
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 5) {
+        return instance.get(`users?page=
+        ${currentPage}&count=${pageSize}`, {
+            withCredentials: true,
+        }).then(response => response.data)
+    },
+    unfollow(userId) {
+        return instance.delete(`follow/
+            ${userId}`, {
+            withCredentials: true,
+        }).then(response => response.data)//??
+    },
+    follow(userId) {
+        return instance.post(`follow/
+                ${userId}`, {
+            withCredentials: true,
+        }).then(response => response.data.resultCode)
+    },
+}
