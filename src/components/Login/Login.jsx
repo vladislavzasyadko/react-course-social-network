57695a722f2562051077854752acb7d7d4efe09f
vlad <gilbert.cook111@gmail.com>
2020-03-28 14:17:33 +0300

    redux form added to complete login form checkbox in firefox is not displayed!

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import {reduxForm, Field} from 'redux-form'
 
 
-const Login = (props) => {
+const LoginForm = (props) => {
 
     return (
-        <div>LOGIN</div>
+        <form onSubmit={props.handleSubmit}>
+            <div><Field component={'input'} name={'login'} placeholder={'Login'} /></div>
+            <div><Field component={'input'} name={'password'} type="password" placeholder={'Password'} /></div>
+            <div><Field component={'input'} name={'rememberMe'} type={'checkbox'} appearance={'checkbox'}/><label> remember me</label></div>
+            <div><button>Login</button></div>
+        </form>
+
+    )
+}
+
+const LoginReduxForm = reduxForm({
+        //unique name for the form
+        form: 'login'})(LoginForm)
+
+const Login = (props) => {
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+
+    return (
+        <div>
+            <div>LOGIN</div>
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
     )
 }
 
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -4,12 +4,14 @@ import {createStore, combineReducers, applyMiddleware } from 'redux';
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
 import thunkMiddleware from 'redux-thunk';
+import {reducer as formReducer} from 'redux-form';
 
 let reducers = combineReducers({
     profileData: profileReducer,
     dialogsData: dialogsReducer,
     usersData: usersReducer,
     auth: authReducer,
+    form: formReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
